8633bf5b07851511256207e5481f98c52c5e89f2
Maureen Oldyck <maureen.oldyck@gmail.com>
2021-02-22 15:04:33 +0100

    Fix function with event error

--- src/script.ts
+++ src/script.ts
@@ -1,27 +1,28 @@
-( () => {
-    const compareUserGuess = (event: any): void => {
+( ():void => {
+    
+    const compareUserGuess:Function = (event:Event) => {
         event.preventDefault();
 
-        const secretNumber: number = generateSecretNumber();
-        const userNumber: unknown = (<HTMLInputElement>document.getElementById('guess')).value;
+        const secretNumber:number = generateSecretNumber();
+        const userNumber:number = (document.getElementById('guess') as HTMLFormElement).valueAsNumber;
 
-        if (secretNumber == (userNumber as number)) {
+        if (secretNumber == userNumber) {
             alert("Awesome! You number " + userNumber + " was correct. You can be named many things, hungry not being one of them.");
         } else {
             alert("Bummer... You guessed " + userNumber + " and the secret number was " + secretNumber + ".");
         }
     }
 
-    const generateSecretNumber = ():number => {
+    const generateSecretNumber:Function = ():number => {
         const secretNumber: number = getRandomArbitrary(1, 22);
 
         return secretNumber;
     }
 
-    const getRandomArbitrary = (min:number, max:number): number => {
+    const getRandomArbitrary:Function = (min:number, max:number): number => {
         return Math.round(Math.random() * (max - min) + min);
     }
 
-    (<HTMLInputElement>document.getElementById('guess-form')).addEventListener("submit", compareUserGuess);
+    (document.getElementById('guess-form') as HTMLFormElement).addEventListener("submit", (compareUserGuess as EventListener));
 
 })();
